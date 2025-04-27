8d7ec32a6f97438741eade8cdcfde1e664b8fcb8
quantulr <35954003+quantulr@users.noreply.github.com>
2025-04-27 17:00:14 +0800

    post release page need auth

--- app/(front)/(posts)/posts/release/new/page.tsx
+++ app/(front)/(posts)/posts/release/new/page.tsx
@@ -1,6 +1,12 @@
 import PostReleaseForm from "@/app/_components/PostReleaseForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (!session) {
+    return redirect("/signin");
+  }
   return (
     <div className="flex min-h-[calc(100dvh_-_72px)] flex-col px-12 py-12 md:flex-row">
       <PostReleaseForm />
